8bd25cfc9a2e317a5d02edcf6f20a9ad900da66f
K4ss4ndr4 <sulemalucasmin@gmail.com>
2019-09-28 21:53:49 -0400

    primer commit

---
+++ database/factories/MinFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Min;
+use Faker\Generator as Faker;
+
+$factory->define(Min::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2019_09_29_014644_create_ingresos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIngresosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingresos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre_ingreso',50);
+            $table->string('descripcion_ingreso',50);
+            $table->date('fecha_ingreso');
+            $table->integer('monto_ingreso');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ingresos');
+    }
+}
